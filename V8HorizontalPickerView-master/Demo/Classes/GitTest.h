28c3016ccd04952d82b843feab329c84280bd361
yoshi <yo44cx@gmail.com>
2013-01-16 20:05:30 +0900

    Gitの利用確認テスト（一時的なTestファイルのため確認後は削除）

---
+++ V8HorizontalPickerView-master/Demo/Classes/GitTest.h
@@ -0,0 +1,13 @@
+//
+//  GitTest.h
+//  HorzPickerTestApp
+//
+//  Created by yoshi on 13/01/16.
+//  Copyright (c) 2013年 V8 Labs, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GitTest : UIViewController
+
+@end
