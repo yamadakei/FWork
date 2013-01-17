7010beec5f6170ff32eddb610349a6455aa934c8
yamadakei <yamadakei1979@gmail.com>
2013-01-17 10:59:45 +0900

    カテゴリ拡張テストプロジェクト作成

---
+++ V8HorizontalPickerView-Expanded/Demo/V8HorizontalPickerView+AddImageInside.m
@@ -0,0 +1,13 @@
+//
+//  V8HorizontalPickerView+AddImageInside.m
+//  HorzPickerTestApp
+//
+//  Created by 山田 慶 on 2013/01/16.
+//  Copyright (c) 2013年 V8 Labs, LLC. All rights reserved.
+//
+
+#import "V8HorizontalPickerView+AddImageInside.h"
+
+@implementation V8HorizontalPickerView (AddImageInside)
+
+@end
